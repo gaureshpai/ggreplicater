1bce53bfba4a2c283a5f1d376d4e8f5fcde82a9d
paigauresh <paigauresh@gmail.com>
2024-05-19 22:37:05 +0530

    Final

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@
 import './components.css'
 export default function Footer() {
     return (
-        <footer className="bg-blue-800 w-full p-4 mt-8">
-            <div className="container mx-auto text-center text-white">
+        <footer className="footer-container">
+            <div className="footer-box">
                 <p>&copy; {new Date().getFullYear()} Gauresh G Pai. All rights reserved.</p>
             </div>
         </footer>
